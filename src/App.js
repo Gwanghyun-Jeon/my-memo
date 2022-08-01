b6ecf6da08cc6e4d9bf2806d92cee9a756857116
GH <gwanghyunjeon@gmail.com>
2022-08-01 19:04:41 +0900

    create react app 시작

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this is my first project "the Memo"
         </a>
       </header>
     </div>
